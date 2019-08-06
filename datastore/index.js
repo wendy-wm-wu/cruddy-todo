7e03f40b9e3c0487ca701dc4df859fce7ed94e66
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-08-05 21:18:11 -0700

    Complete deleting a Todo

--- datastore/index.js
+++ datastore/index.js
@@ -62,14 +62,17 @@ exports.update = (id, text, callback) => {
 
 exports.delete = (id, callback) => {
   var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
+
+
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
